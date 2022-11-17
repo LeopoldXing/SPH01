2ffb346ed10a6579451f83f4484be18de65d9d6d
DiscoBroccoli <19515DearClem@gmail.com>
2022-11-17 16:28:19 +0800

    Day13 尚品汇单点登录
    1. 用户登录
    2. 用户退出登录
    3. 网关对请求进行处理
       3.1 放行静态资源
       3.2 拦截访问内部接口的外部请求

--- gmall-parent/server-gateway/src/main/java/com/hilda/gateway/filter/GlobalAuthenticationFilter.java
+++ gmall-parent/server-gateway/src/main/java/com/hilda/gateway/filter/GlobalAuthenticationFilter.java
@@ -1,15 +1,21 @@
 package com.hilda.gateway.filter;
 
+import com.alibaba.fastjson.JSONObject;
+import com.hilda.common.result.Result;
+import com.hilda.common.result.ResultCodeEnum;
 import com.hilda.gateway.config.AuthUrlProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Component
@@ -21,7 +27,6 @@ public class GlobalAuthenticationFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
-        System.out.println("==============请求开始==============");
 
         // 1. 获取请求的路径
         String path = request.getURI().getPath();
@@ -43,12 +48,24 @@ public class GlobalAuthenticationFilter implements GlobalFilter {
         long innerCount = innerUrl.parallelStream().filter(pattern -> antPathMatcher.match(pattern, path)).count();
         if (innerCount > 0) {
             System.out.println("拦截尝试访问内部调用接口的外部请求");
-            return null;
+            ServerHttpResponse response = exchange.getResponse();
+            return out(response, ResultCodeEnum.PERMISSION);
         }
 
         Mono<Void> filterMono = chain.filter(exchange);
 
         return filterMono;
+    }
+
+    // 接口鉴权失败返回数据
+    private Mono<Void> out(ServerHttpResponse response, ResultCodeEnum resultCodeEnum) {
+        // 返回用户没有权限登录
+        Result<Object> result = Result.build(null, resultCodeEnum);
+        byte[] bits = JSONObject.toJSONString(result).getBytes(StandardCharsets.UTF_8);
+        DataBuffer wrap = response.bufferFactory().wrap(bits);
+        response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+        // 输入到页面
+        return response.writeWith(Mono.just(wrap));
     }
 
 }
